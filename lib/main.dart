1d6a8fdb12ce973b8d729b160d03568246e3e778
git-tom728 <a.jam.hieing.koy@gmail.com>
2025-03-29 13:11:09 +0900

    FireBaseの設定ファイルを組み込んでデモを実行成功すたとこまで完了

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); // Firebase 初期化前に必要
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
