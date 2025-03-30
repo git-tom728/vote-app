b5efdfa110601eb4bcdfe7fac9e9e6f4477584a3
git-tom728 <a.jam.hieing.koy@gmail.com>
2025-03-30 10:49:31 +0900

    ログイン画面を実装　※未テスト下のコミットを微修正

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
-import 'login_page.dart'; // ← これを追加！
+import 'login_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized(); // Firebase 初期化前に必要
